aedbe7474859e17ebeca9695baecfb56fcbff4e9
Oleksandr <oleksandr.maksymenko@space-whale.io>
2022-06-16 11:35:40 +0300

    improve

--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import {GetServerSidePropsContext} from 'next';
+import {useForm} from 'react-hook-form';
 import {checkSession} from 'src/checkSession';
 import {wrapper} from 'src/store';
 import {companiesApi} from 'src/api';
+import {TextField, Button} from '@mui/material';
 
 const Company = () => {
-  return <div>Company</div>;
+  const {register, handleSubmit} = useForm();
+
+  const submit = (formState: {[key: string]: string}) => {
+    console.log(formState);
+  };
+
+  return (
+    <form onSubmit={e => handleSubmit(submit)(e)}>
+      <TextField
+        {...register('company', {
+          minLength: 2,
+          maxLength: 10,
+          required: true,
+        })}
+        placeholder='company name'
+      />
+      <TextField
+        {...register('description', {
+          minLength: 2,
+          maxLength: 10,
+          required: true,
+        })}
+        placeholder='description'
+      />
+      <Button type='submit'>Submit</Button>
+    </form>
+  );
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
